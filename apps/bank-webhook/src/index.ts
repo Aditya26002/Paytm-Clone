e2d509ad41fbc78941146d11698f67ac4b34fa38
Aditya <aditya2012058@akgec.ac.in>
2024-12-18 21:31:28 +0530

    Add Zod validation to HDFC webhook and improve error handling

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,30 +1,42 @@
 import express from "express";
 import db from "@repo/db/client";
-
+import z from "zod";
 const app = express();
 
+app.use(express.json());
+
 app.post("/hdfcWebhook", async (req, res) => {
-  //TODO: Add zod validation here?
+  const HDFCWebhookSchema = z.object({
+    token: z.string().min(1),
+    user_identifier: z.number().min(1),
+    amount: z.string().min(1),
+  });
+
+  const parsed = HDFCWebhookSchema.safeParse(req.body);
+  if (!parsed.success) {
+    console.error("Error parsing request body: ", parsed.error);
+    return res.status(400).send({ message: "Bad Request" });
+  }
+
   const paymentInformation = {
-    token: req.body.token,
-    userId: req.body.user_identifier,
-    amount: req.body.amount,
+    token: parsed.data.token,
+    userId: parsed.data.user_identifier,
+    amount: parsed.data.amount,
   };
 
   try {
     await db.$transaction([
-      db.balance.update({
+      db.balance.updateMany({
         where: {
-          userId: Number(paymentInformation.userId),
+          userId: paymentInformation.userId,
         },
         data: {
           amount: {
-            // You can also get this from your DB
             increment: Number(paymentInformation.amount),
           },
         },
       }),
-      db.onRampTransaction.update({
+      db.onRampTransaction.updateMany({
         where: {
           token: paymentInformation.token,
         },
@@ -39,10 +51,12 @@ app.post("/hdfcWebhook", async (req, res) => {
     });
   } catch (e) {
     console.error(e);
-    res.status(411).json({
+    res.status(500).json({
       message: "Error while processing webhook",
     });
   }
 });
 
-app.listen(3003);
+app.listen(3003, () => {
+  console.log("Server is running on port 3003");
+});
